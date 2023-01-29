fd9f01fd1056037616bd4d861bdfc4fd34e11e7d
AbdulHamid Merii <ah_merii@outlook.com>
2023-01-29 22:35:34 +0000

    add script to setup entire environment and download all dependencies

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# check and install any updates
+echo"Updating packages"
+sudo apt -y update
+sudo apt -y upgrade
+
+# install packages
+echo "Installing the following packages:"
+cat packages.txt
+xargs -a packages.txt | (yes | sudo apt install)
+
+# install tools and programming languages
+bash install/tools.sh
+
+# install helix editor language server protocols 
+bash install/helix-lsp.sh
+
+# empty /tmp dir
+sudo find /tmp/* -exec rm -rf {} + 
+
+# 
