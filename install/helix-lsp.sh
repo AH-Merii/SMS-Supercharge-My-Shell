ef56ac9d88263a49c19ed0f73c3f57421dd14ac9
AbdulHamid Merii <ah_merii@outlook.com>
2023-01-29 21:52:44 +0000

    add script to install helix lsps programaticaly

---
+++ install/helix-lsp.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+# source helper functions
+source "../scripts/helper-funcs.sh"
+
+BINARY_HOME="${HOME}/.local/bin"
+DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+
+echo "Installing Language Servers for Helix Editor:"
+
+# Work in a throwaway temporary directory so as not to pollute the file system.
+temporaryDirectory="/tmp/helix-editor-language-server-installer"
+mkdir -p "${temporaryDirectory}"
+pushd "${temporaryDirectory}"
+
+# Bash
+echo "  • Bash (bash-language-server)"
+npm i -g bash-language-server
+
+# HTML, JSON, JSON schema
+echo "  • HTML, JSON, and JSON schema (vscode-langservers-extracted)"
+npm i -g vscode-langservers-extracted
+
+# JavaScript (via TypeScript)
+echo "  • JavaScript (typescript, typescript-language-server)"
+npm install -g typescript typescript-language-server
+
+# Python
+sudo apt install python3-pylsp
+
+# Terraform HCL
+sudo apt install terraform-ls
+
+# Latex
+echo "  • Latex (texlab)"
+cargo install texlab
+
+# Markdown
+echo "  • Markdown (marksman)"
+repo="artempyanykh/marksman"
+release_url=$(curl -L "https://api.github.com/repos/$repo/releases/latest" | \
+    jq -r ".assets[].browser_download_url" | \
+    grep -Pi "linux")
+curl -Lo "marksman" $release_url \
+&& chmod +x marksman \
+&& mv marksman "$HOME/.local/bin"
+
+# TOML
+cargo install taplo-cli --locked --features lsp
+
+# Go
+go install golang.org/x/tools/gopls@latest
+
+# Docker
+npm install -g dockerfile-language-server-nodejs
+
+# Rust
+# Automatically installed by rustup during rust installation
+
+# Clean up.
+popd
+rm -rf "${temporaryDirectory}"
+
+echo "Done."
