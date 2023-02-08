7db420e6cfde104944f263b52171670f76e14ee9
AbdulHamid Merii <ah_merii@outlook.com>
2023-02-08 15:16:04 +0000

    fix nix installation script

--- setup.sh
+++ setup.sh
@@ -9,15 +9,13 @@ NIX_PACKAGES_FILE="$(pwd)/programs/nix-packages.txt"
 echo "Updating packages"
 sudo apt -y update
 sudo apt -y upgrade
+sudo apt install build-essential zsh
 
 # check if nix is installed on the system
 if ! (which nix); then 
-  echo -e "\033[31mError: An error occurred\033[0m" >&2
-  echo -e "\033[31mnix is not installed on the system\033[0m" >&2
-  echo -e "\033[36mrun ./install/nix.sh to install nix package manager\033[0m"
-  echo -e "\033[36mrestart your terminal after installing nix\033[0m"
-  echo -e "\033[36mre-run this script after you have restarted your terminal\033[0m" 
-  exit 1
+  # install nix package manager using helper script
+  ./install/nix.sh
+  source /home/amerii/.nix-profile/etc/profile.d/nix.sh
 fi
 
 echo "Updating Nix packages"
@@ -25,11 +23,15 @@ nix-channel --update
 
 # check if file containing list of packages exists
 if [ -s $NIX_PACKAGES_FILE ]; then
+  # allow the installation of proprietary packages
+  export NIXPKGS_ALLOW_UNFREE=1
+  export NIXPKGS_ALLOW_BROKEN=1
   echo "Downloading Nix packages from $NIX_PACKAGES_FILE"
   echo "Installing the following packages:"
   cat $NIX_PACKAGES_FILE
   # the command below adds "nixpkgs." to the start of each packages and removes all comments and newlines
-  nix-env -iA $(sed 's/^/nixpkgs./' $NIX_PACKAGES_FILE | grep -o '^[^#]*' | tr '\n' ' ')
+  cat $NIX_PACKAGES_FILE | grep -o '^[^#]*' > /tmp/nix-packages.txt
+  nix-env -iA $(sed 's/^/nixpkgs./' "/tmp/nix-packages.txt" | tr '\n' ' ') 
 else # exit if package file does not exist
   echo $NIX_PACKAGES_FILE is empty or does not exist
   exit 1
@@ -39,14 +41,14 @@ fi
 which zsh && chsh -s $(which zsh) || echo zsh not found
 
 # create symlinks to dotfiles using stow
-stow */
+stow */ -t ~
+
+# export environment variables from .zshenv
+source "~/.zshenv"
 
 # simlink and source scripts file
 ln -s scripts "$ZDOTDIR/scripts"
 source "$ZDOTDIR/scripts"
-
-# export environment variables from .zshenv
-source "~/.zshenv"
 
 # install tools programming languages and editors
 chmod +x install/tools.sh
