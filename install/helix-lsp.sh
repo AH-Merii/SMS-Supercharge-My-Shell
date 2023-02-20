c9eb81c4a4c78c5703e66309471315488961ee20
AbdulHamid Merii <ah_merii@outlook.com>
2023-02-20 15:44:59 +0000

    Fix terraform lsp installation

--- install/helix-lsp.sh
+++ install/helix-lsp.sh
@@ -33,6 +33,11 @@ sudo apt install python3-pylsp
 
 # Terraform HCL
 echo "  • Terraform (terraform-ls)"
+sudo apt install gpg
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
 sudo apt install terraform-ls
 
 # Latex
