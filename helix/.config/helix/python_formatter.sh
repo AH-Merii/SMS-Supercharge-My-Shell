a281d380dcbb3aaa85c57f1cbbfb855cbd822df0
AH-Merii <ah_merii@outlook.com>
2023-08-23 10:12:31 +0100

    Add isort to helix autoformatting

--- helix/.config/helix/python_formatter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Check if there are any arguments
-if [ "$#" -eq 0 ]; then
-    isort --profile black . && black . "--quiet","--line-length=88"
-    exit 1
-fi
-
-isort --profile black $1 && black $1 "--quiet","--line-length=88"
