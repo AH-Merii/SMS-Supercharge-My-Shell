77aff96963f505a021a4df65ea971637bf82776d
AbdulHamid Merii <ah_merii@outlook.com>
2024-01-26 14:31:19 +0000

    which-key initial setup

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -3,7 +3,7 @@ local M = {
   event = "VeryLazy",
   init = function()
     vim.o.timeout = true
-    vim.o.timeoutlen = 300
+    vim.o.timeoutlen = 100
   end,
   opts = {
     -- your configuration comes here
@@ -11,5 +11,54 @@ local M = {
     -- refer to the configuration section below
   }
 }
+
+function M.config()
+  local mappings = {
+    q = { "<cmd>confirm q<CR>", "Quit" },
+    h = { "<cmd>nohlsearch<CR>", "NOHL" },
+    [";"] = { "<cmd>tabnew | terminal<CR>", "Term" },
+    v = { "<cmd>vsplit<CR>", "Split" },
+    b = { name = "Buffers" },
+    d = { name = "Debug" },
+    f = { name = "Find" },
+    g = { name = "Git" },
+    l = { name = "LSP" },
+    p = { name = "Plugins" },
+    t = { name = "Test" },
+    a = {
+      name = "Tab",
+      n = { "<cmd>$tabnew<cr>", "New Empty Tab" },
+      N = { "<cmd>tabnew %<cr>", "New Tab" },
+      o = { "<cmd>tabonly<cr>", "Only" },
+      h = { "<cmd>-tabmove<cr>", "Move Left" },
+      l = { "<cmd>+tabmove<cr>", "Move Right" },
+    },
+    T = { name = "Treesitter" },
+  }
+
+  local which_key = require "which-key"
+  which_key.setup {
+    plugins = {
+      marks = true,
+      registers = true,
+      spelling = {
+        enabled = true,
+        suggestions = 20,
+      },
+    },
+    window = {
+      border = "rounded",
+      position = "bottom",
+      padding = { 2, 2, 2, 2 },
+    },
+  }
+
+  local opts = {
+    mode = "n", -- NORMAL mode
+    prefix = "<leader>",
+  }
+
+  which_key.register(mappings, opts)
+end
 
 return M
