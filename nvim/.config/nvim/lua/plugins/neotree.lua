6d810fcb5004270df779915fd6ffaa7ee6b8c895
AbdulHamid Merii <ah_merii@outlook.com>
2024-01-29 09:52:45 +0000

    Add option to yank path from neotree

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -1,16 +1,55 @@
 local M = {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-    "3rd/image.nvim",
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+		"3rd/image.nvim",
+	},
 }
 
 function M.config()
-  vim.keymap.set('n', '<leader>n', ':Neotree toggle<CR>')
+	vim.keymap.set("n", "<leader>n", ":Neotree toggle<CR>")
+	require("neo-tree").setup({
+		window = {
+			mappings = {
+				["Y"] = function(state)
+					local node = state.tree:get_node()
+					local filepath = node:get_id()
+					local filename = node.name
+					local modify = vim.fn.fnamemodify
+
+					local results = {
+						filepath,
+						modify(filepath, ":."),
+						modify(filepath, ":~"),
+						filename,
+						modify(filename, ":r"),
+						modify(filename, ":e"),
+					}
+
+					vim.ui.select(results, { prompt = "Choose to copy to clipboard or Quit with q:" }, function(choice)
+						if not choice then
+							return -- Do nothing and exit if 'Quit' is selected or if there's no choice (e.g., user pressed escape)
+						end
+
+						local i = tonumber(choice:sub(1, 1))
+						if not i then
+							return
+						end -- Additional check in case the choice is not a number
+						local result = results[i]
+						if not result then
+							return
+						end -- Check if the result is valid
+
+						vim.fn.setreg('"', result)
+						vim.notify("Copied: " .. result)
+					end)
+				end,
+			},
+		},
+	})
 end
 
 return M
