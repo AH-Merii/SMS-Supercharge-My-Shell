aafc69a206eea859ed2a3bcfa54cde9b2e10996b
AbdulHamid Merii <ah_merii@outlook.com>
2024-01-30 11:14:24 +0000

    Fix neotree yank feature

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -9,44 +9,53 @@ local M = {
 	},
 }
 
+local function copy_path(state)
+	-- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
+	-- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+	local node = state.tree:get_node()
+	local filepath = node:get_id()
+	local filename = node.name
+	local modify = vim.fn.fnamemodify
+
+	local results = {
+		filepath,
+		modify(filepath, ":."),
+		modify(filepath, ":~"),
+		filename,
+		modify(filename, ":r"),
+		modify(filename, ":e"),
+	}
+
+	vim.ui.select({
+		"1. Absolute path: " .. results[1],
+		"2. Path relative to CWD: " .. results[2],
+		"3. Path relative to HOME: " .. results[3],
+		"4. Filename: " .. results[4],
+		"5. Filename without extension: " .. results[5],
+		"6. Extension of the filename: " .. results[6],
+	}, { prompt = "Choose to copy to clipboard:" }, function(choice)
+		if choice then
+			local i = tonumber(choice:sub(1, 1))
+			if i then
+				local result = results[i]
+				vim.fn.setreg('"', result)
+				vim.notify("Copied: " .. result)
+			else
+				vim.notify("Invalid selection")
+			end
+		else
+			vim.notify("Selection cancelled")
+		end
+	end)
+end
+
 function M.config()
 	vim.keymap.set("n", "<leader>n", ":Neotree toggle<CR>")
 	require("neo-tree").setup({
 		window = {
 			mappings = {
-				["Y"] = function(state)
-					local node = state.tree:get_node()
-					local filepath = node:get_id()
-					local filename = node.name
-					local modify = vim.fn.fnamemodify
-
-					local results = {
-						filepath,
-						modify(filepath, ":."),
-						modify(filepath, ":~"),
-						filename,
-						modify(filename, ":r"),
-						modify(filename, ":e"),
-					}
-
-					vim.ui.select(results, { prompt = "Choose to copy to clipboard or Quit with q:" }, function(choice)
-						if not choice then
-							return -- Do nothing and exit if 'Quit' is selected or if there's no choice (e.g., user pressed escape)
-						end
-
-						local i = tonumber(choice:sub(1, 1))
-						if not i then
-							return
-						end -- Additional check in case the choice is not a number
-						local result = results[i]
-						if not result then
-							return
-						end -- Check if the result is valid
-
-						vim.fn.setreg('"', result)
-						vim.notify("Copied: " .. result)
-					end)
-				end,
+				["Y"] = copy_path,
+				-- Additional mappings
 			},
 		},
 	})
