6f2c4e361cfe2576397b480beb80f141dda676c7
AbdulHamid Merii <ah_merii@outlook.com>
2024-02-05 10:59:17 +0000

    Configure displaying incomplete commands

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -2,10 +2,15 @@ local M = {
 	"nvim-lualine/lualine.nvim",
 }
 
+vim.opt.showcmdloc='statusline'
+
 function M.config()
   require("lualine").setup({
     options = {
       theme = "catppuccin",
+    },
+    sections = {
+      lualine_c = {'%S'},
     },
   })
 end
