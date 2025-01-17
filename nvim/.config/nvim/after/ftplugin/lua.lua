380986018b073ee2173c2d4b52e3477863956e80
AbdulHamid Merii <ah_merii@outlook.com>
2025-01-17 17:32:44 +0000

    Add after/ftplugin config for lua

--- nvim/.config/nvim/after/ftplugin/lua.lua
+++ nvim/.config/nvim/after/ftplugin/lua.lua
@@ -1,12 +1,12 @@
-local function set_lua_keymaps()
+local function set_keymaps()
 	local opts = { noremap = true, silent = false, buffer = true } -- buffer = true, ensures that it is only applied for current buffer
 	local keymap = vim.keymap.set
 
 	keymap("n", "<space>x", ":.lua<CR>", vim.tbl_extend("force", opts, { desc = "Execute current line" }))
-	keymap("v", "<space>x", ":lua<CR>", vim.tbl_extend("force", opts, { desc = "Execute current line" }))
+	keymap("v", "<space>x", ":lua<CR>", vim.tbl_extend("force", opts, { desc = "Execute current selection" }))
 end
 
-set_lua_keymaps()
+set_keymaps()
 
 local set = vim.opt_local
 
