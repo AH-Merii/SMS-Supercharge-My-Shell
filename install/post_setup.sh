e51862d6aac529c26b3b727cd42a8d8c882183b0
AbdulHamid Merii <ah_merii@outlook.com>
2025-04-22 08:11:24 +0100

    Add check to see if you are already authenticated with github

--- install/post_setup.sh
+++ install/post_setup.sh
@@ -10,11 +10,22 @@ source "${SCRIPT_DIR}/show_progress.sh"
 # Main function to orchestrate the setup
 main() {
   run_with_progress --input-message "Setting Up Vale" -c "vale sync"
-  run_command --input-message "Authenticating with GitHub" --show-output \
-    --success-message "Authentication with Github Successful" -c "gh auth login" &&
-    # only attempt to install extension if auth is succesful
-    run_with_progress --input-message "Installing gh-notify extension" \
+
+  # check if you are authenticated with github if not authenticate github
+  run_command --input-message "Checking GitHub Authentication" \
+    --success-message "GitHub already authenticated" \
+    -c "gh auth status" >/dev/null 2>&1
+  local github_auth_status=$?
+
+  if [[ "${github_auth_status}" -ne 0 ]]; then
+    run_command --input-message "Authenticating with GitHub" --show-output \
+      --success-message "Authentication with Github Successful" -c "gh auth login"
+    local github_auth_status=$?
+    #only attempt to install extension if auth is succesful
+    [[ "${github_auth_status}" -ne 0 ]] && run_with_progress --input-message "Installing gh-notify extension" \
       -c "gh extension install meiji163/gh-notify"
+  fi
+
 }
 
 main
