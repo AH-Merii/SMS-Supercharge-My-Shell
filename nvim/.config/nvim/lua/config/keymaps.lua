acdfee0aff95f07e77dd511cff459f816e70bdf1
AbdulHamid Merii <ah_merii@outlook.com>
2025-11-04 14:14:21 +0000

    Fix jitter when visual indenting
    This fixes the jitter caused by snacks animate redrawing the highlight
    every time I tab, this is because of the scroll-back being set to 999.

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -21,9 +21,42 @@ keymap("n", "<leader>q", ":q!<CR>", silent_desc("Quit without saving"))
 keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, desc = "Move up (respects wraps)" })
 keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, desc = "Move down (respects wraps)" })
 
--- Better indenting
-keymap("v", "<", "<gv", desc("Indent left (stay in visual)"))
-keymap("v", ">", ">gv", desc("Indent right (stay in visual)"))
+-- Better indenting (with temporary Snacks indent animation disable)
+keymap("v", "<", function()
+  local ok, snacks = pcall(require, "snacks")
+  if ok and snacks and snacks.indent then
+    local buf = vim.api.nvim_get_current_buf()
+    local prev_state = vim.b[buf].snacks_indent_animate
+    vim.b[buf].snacks_indent_animate = false
+
+    -- Perform normal indent and stay in visual mode
+    vim.cmd("normal! <gv")
+
+    -- Restore animation after short delay
+    vim.defer_fn(function()
+      vim.b[buf].snacks_indent_animate = prev_state
+    end, 30)
+  else
+    vim.cmd("normal! <gv")
+  end
+end, desc("Indent left"))
+
+keymap("v", ">", function()
+  local ok, snacks = pcall(require, "snacks")
+  if ok and snacks and snacks.indent then
+    local buf = vim.api.nvim_get_current_buf()
+    local prev_state = vim.b[buf].snacks_indent_animate
+    vim.b[buf].snacks_indent_animate = false
+
+    vim.cmd("normal! >gv")
+
+    vim.defer_fn(function()
+      vim.b[buf].snacks_indent_animate = prev_state
+    end, 30)
+  else
+    vim.cmd("normal! >gv")
+  end
+end, desc("Indent right"))
 
 -- Paste over currently selected text without yanking it
 keymap("v", "p", '"_dP', silent_desc("Paste without yanking"))
