d7471a8e10f367a8b53ceacf3963bb1c8035392b
AbdulHamid Merii <ah_merii@outlook.com>
2025-11-10 04:44:08 +0000

    Add color-highlight preview plugin

--- nvim/.config/nvim/lua/plugins/extras.lua
+++ nvim/.config/nvim/lua/plugins/extras.lua
@@ -87,4 +87,22 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {},
   },
+  {
+    "brenoprata10/nvim-highlight-colors",
+    opts = {
+      render = "virtual",
+      virtual_symbol = "",
+      ---Set virtual symbol suffix (defaults to '')
+      virtual_symbol_prefix = " ",
+
+      ---Set virtual symbol suffix (defaults to ' ')
+      virtual_symbol_suffix = "",
+      ---Set virtual symbol position()
+      ---@usage 'inline'|'eol'|'eow'
+      ---inline mimics VS Code style
+      ---eol stands for `end of column` - Recommended to set `virtual_symbol_suffix = ''` when used.
+      ---eow stands for `end of word` - Recommended to set `virtual_symbol_prefix = ' ' and virtual_symbol_suffix = ''` when used.
+      virtual_symbol_position = "eol",
+    },
+  },
 }
