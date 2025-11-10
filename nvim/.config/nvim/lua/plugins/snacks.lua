40781fc00c24e94bb84d43408a946d0c10a5763b
AbdulHamid Merii <ah_merii@outlook.com>
2025-11-10 00:54:03 +0000

    Enable folds to show in statuscolumn snacks

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -15,7 +15,13 @@ return {
     quickfile = { enabled = true },
     scope = { enabled = true },
     scroll = { enabled = true },
-    statuscolumn = { enabled = true },
+    statuscolumn = {
+      enabled = true,
+      folds = {
+        open = true, -- show open fold icons
+        git_hl = false, -- use Git Signs hl for fold icons
+      },
+    },
     words = { enabled = true },
   },
 
